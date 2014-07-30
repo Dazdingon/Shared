50b8d8e43d79275d2a1e2883c91d6d33ef1f1a87
Dazdingon <nlareman@gmail.com>
2014-07-30 19:07:53 +0200

    ...

--- Scripts/FPS_CameraToView.cs
+++ Scripts/FPS_CameraToView.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class FPS_CameraToView : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
-	}
+	public CharacterStats stats;
+	public Transform cameraTransform;
 	
 	// Update is called once per frame
 	void Update () {
 	
+		RotateCamera();
+	}
+	
+	// RotateCamera
+	void RotateCamera () {
+	
+		cameraTransform.rotation = Quaternion.Euler(stats.viewAngles.y, stats.viewAngles.x, 0);
 	}
 }
