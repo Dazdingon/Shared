2feb31740252c5c72d0bd8074bb85bb5e2b2db49
Dazdingon <nlareman@gmail.com>
2014-07-30 18:25:47 +0200

    Initial

---
+++ Scripts/CharacterStats.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour {
+	
+	// Runtime movement
+	public float horizontal;
+	public float vertical;
+	public Vector3 lookVector;
+	public Vector3 moveVector;
+	public Vector3 forceVector;
+	
+	
+	public bool run = false;
+	public bool jump = false;
+	public bool crouch = false;
+	
+	// Stats
+	public float moveSpeed = 10f;
+	public float jumpForce = 10f;
+	public float fallSpeed = 10f;
+
+}
---
+++ Scripts/FPS_MovementInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPS_MovementInput : MonoBehaviour {
+	
+	public CharacterStats stats;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		
+	}
+}
