5095e872cd752f329b9f7b2cf455b67bc7d816f0
Weylin <wprenison@gmail.com>
2014-08-01 20:52:07 +0200

    UI Inventory Slot Graphics & Pick up

--- Scripts/Resource.cs
+++ Scripts/Resource.cs
@@ -4,16 +4,15 @@ using System.Collections;
 public class Resource{
 	
 	public string resourceType;
-	public string resourceIconDir;
-	public Texture2D resourceIcon;
+	public GameObject resourceIcon;
 	public int qty;
 	
 	public Resource(string resourceType, int qty)
 	{
 		this.resourceType = resourceType;
 		this.qty = qty;
-		resourceIconDir = "Icons/" + resourceType;
-		resourceIcon = new Texture2D (100, 100);
+		resourceIcon = GameObject.Find (resourceType);
+		resourceIcon.renderer.enabled = true;
 	}
 	
 	//Methods on how to use wood/stone/ect
