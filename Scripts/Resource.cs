cdbd9842948aa4fe39591970d7a8be3791adf6b1
Weylin <wprenison@gmail.com>
2014-08-01 17:51:14 +0200

    Added Classes for resource system, 2 game objs
    Classes:
    -ResourceObj
    Used on any game object that should be seen as a resource ie. Tree ,
    Rock ect
    
    -Resource
    The actual resource the player will pick up
    ie. Wood, Stone, ect.
    
    Game Objs
    -ResourceObj
    Empty game object to caontain a all game objs that are seen as resources
    -Tree
    -Rock

---
+++ Scripts/Resource.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resource : MonoBehaviour {
+	
+	public string resourceType;
+	public string resourceIconDir;
+	public Texture2D resourceIcon;
+	public int qty;
+	
+	public Resource(string resourceType, int qty)
+	{
+		this.resourceType = resourceType;
+		this.qty = qty;
+		resourceIconDir = "Icons/" + resourceType;
+		resourceIcon = new Texture2D (100, 100);
+	}
+	
+	//Methods on how to use wood/stone/ect
+}
---
+++ Scripts/ResourceObj.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceObj : MonoBehaviour {
+	
+	public string objType = "";
+	public string resourceType;	
+	public int resourceQty;
+	
+	void Start()
+	{
+		objType = this.gameObject.name;
+		
+		switch (objType) 
+		{
+		case "Tree":	
+			resourceType = "Wood";
+			resourceQty = 5;
+			break;
+			
+		case "Rock":
+			resourceType = "Stone";
+			resourceQty = 2;
+			break;
+		}
+	}
+	
+	public Resource getResource()
+	{
+		Resource res = new Resource (resourceType, resourceQty);
+		return res;
+	}
+}
