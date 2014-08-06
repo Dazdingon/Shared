0259412b38b201c840c7ed4c782f263bb78de8e9
Dazdingon <nlareman@gmail.com>
2014-08-06 20:55:39 +0200

    Object placement (Working Test)
    Place Object : Left mouse button
    Rotate placement : Mouse scroll wheel

--- Scripts/Character_ObjectPlacing.cs
+++ Scripts/Character_ObjectPlacing.cs
@@ -5,7 +5,12 @@ public class Character_ObjectPlacing : MonoBehaviour {
 	
 	public CharacterStats stats;
 	
-	public GameObject[] objectTemplates;
+	public PlaceableObject[] placeableObjects;
+	
+	public PlaceableObject placeableInstance;
+	
+	private Transform objectInstance;
+	private float angleOffset = 90f;
 	
 	void Start () {
 	
@@ -13,5 +18,44 @@ public class Character_ObjectPlacing : MonoBehaviour {
 
 	void Update () {
 	
+		if(!objectInstance){
+			
+			objectInstance = ((GameObject)(GameObject.Instantiate(placeableObjects[0].placementObject))).transform;
+			placeableInstance = objectInstance.GetComponentInChildren<PlaceableObject>();
+		}
+		
+		if(stats.raycastPosetive){
+			
+			if(stats.raycastInfo.distance < stats.interactDistance && stats.raycastInfo.point.y < 0.1f){
+
+				ShowPalceable();
+			}
+			else{
+				
+				HidePalceable();
+			}
+		}
+		else{
+			
+			HidePalceable();
+		}
+		
+		angleOffset += Input.GetAxis("Mouse ScrollWheel") * 90f;
+	}
+	
+	void ShowPalceable () {
+	
+		objectInstance.position = stats.raycastInfo.point;
+		objectInstance.rotation = Quaternion.Euler(0, stats.viewAngles.x + angleOffset, 0);
+		
+		if(Input.GetMouseButtonDown(0)){
+			
+			placeableInstance.PlaceObject();
+		}
+	}
+	
+	void HidePalceable () {
+	
+		objectInstance.position = Vector3.down * 5f;
 	}
 }
---
+++ Scripts/PlaceableObject.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceableObject : MonoBehaviour {
+	
+	public GameObject placementObject;
+	public GameObject placeableTemplate;
+	
+	public Renderer renderTarget;
+	public Color color_Placeable = Color.white;
+	public Color color_Unplaceable = Color.white;
+	
+	public bool isPlaceable = false;
+	
+
+	public void PlaceObject () {
+		
+		if(isPlaceable){
+			
+			Transform newInstance;
+				
+			newInstance = ((GameObject)(GameObject.Instantiate(placeableTemplate))).transform;
+			newInstance.position = placementObject.transform.position;
+			newInstance.rotation = placementObject.transform.rotation;
+		}
+	}
+	
+
+	void Update () {
+	
+		if(isPlaceable){
+			
+			renderTarget.material.color = color_Placeable;
+		}
+		else {
+			
+			renderTarget.material.color = color_Unplaceable;
+		}
+		//this.collider.rigidbody.WakeUp();
+	}
+	
+	void FixedUpdate () {
+		
+		isPlaceable = true;
+	}
+	
+	//void OnTriggerStay(Collider other) {
+	void OnTriggerStay() {
+	
+		isPlaceable = false;
+	}
+}
