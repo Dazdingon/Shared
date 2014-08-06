dbd5ff334ddb88a7bc2282778a9489536e11ec68
Dazdingon <nlareman@gmail.com>
2014-08-06 19:33:25 +0200

    Making game ready for object placement

---
+++ Scripts/Character_ObjectPlacing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_ObjectPlacing : MonoBehaviour {
+	
+	public CharacterStats stats;
+	
+	public GameObject[] objectTemplates;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
