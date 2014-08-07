45c506590dd8afd695cf673053c80b862d4d992c
Weylin <wprenison@gmail.com>
2014-08-07 20:56:33 +0200

    ChildrenDetacher
    ChildrenDetacher
    used on 2halves stone and 2halves tree.
    
    Detaches children from parent then destroys parent

---
+++ Scripts/ChildrenDetacher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildrenDetacher : MonoBehaviour {
+
+	public GameObject parentObject;
+
+	void Start()
+	{
+		parentObject.transform.DetachChildren ();
+		Destroy (parentObject);
+	}
+}
